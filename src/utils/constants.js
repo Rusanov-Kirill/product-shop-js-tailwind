1819b162cbaf5eb8607f2158c9371a5c3a132e0a
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-08-21 03:01:24 +0600

    chore(utils): add new constants for hero section comp

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,14 @@
-export const ICON_SIZE = 'w-6 h-6';
+export const ICON_SIZE = {
+  small: 'w-4 h-4',
+  medium: 'w-6 h-6',
+  large: 'w-8 h-8',
+  xl: 'w-10 h-10',
+  xxl: 'w-12 h-12',
+};
 
 export const NAV_LINKS = ['Home', 'Products', 'About', 'Contact'];
 
 export const ASSETS = {
   logo: '/src/assets/images/NovaCartLogo.png',
+  hero_background: '../assets/images/HeroSectionBg.png',
 };
