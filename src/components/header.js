96cae9fe751902ce621dbef1944fd011843a266d
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-08-29 01:26:11 +0600

    style(header): make header fully responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,34 @@
-import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faShoppingCart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import { createElement, createElementWithChildren } from '../utils/dom.js';
 import { createFontAwesomeIcon } from '../utils/createIcon.js';
 import { ICON_SIZE, NAV_LINKS, ASSETS } from '../utils/constants.js';
 
 export function Header() {
-    const logo = createElement('img', 'w-64 h-auto select-none cursor-pointer');
+    const menuIcon = createFontAwesomeIcon(faBars, `${ICON_SIZE.medium} text-gray-900 hover:text-blue-700 cursor-pointer`);
+    const menuBtn = createElementWithChildren('button', 'flex items-center text-gray-900 p-2 h-fit self-center lg:hidden', [menuIcon]);
+
+    const logo = createElement('img', 'w-auto h-16 justify-self-center self-center select-none cursor-pointer md:h-20 lg:h-22');
     logo.src = ASSETS.logo;
     logo.alt = 'NovaCart Logo';
 
-    const nav = createElementWithChildren('nav', 'flex justify-beetween items-center gap-12 font-medium text-lg', [
+    const nav = createElementWithChildren('nav', 'hidden flex justify-between items-center font-medium text-lg lg:flex lg:gap-6 xl:gap-12', [
         ...NAV_LINKS.map(linkText => {
             const link = createElement('a', `flex p-2 cursor-pointer hover:text-blue-700 ${linkText === 'Home' ? 'relative text-blue-700 after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-blue-700' : 'text-gray-900'}`, linkText);
             return link;
         })
     ]);
 
+    const loupeIcon = createFontAwesomeIcon(faMagnifyingGlass, `${ICON_SIZE.medium} text-gray-900 cursor-pointer hover:text-blue-700`);
+    const loupeBtn = createElementWithChildren('button', 'flex items-center justify-center text-gray-900 p-2', [loupeIcon]);
     const userIcon = createFontAwesomeIcon(faUser, `${ICON_SIZE.medium} text-gray-900 cursor-pointer group-hover:text-blue-700`);
-    const userText = createElement('span', 'text-gray-900 p-2 cursor-pointer group-hover:text-blue-700', 'Sign In');
-    const userWrapper = createElementWithChildren('div', 'group flex gap-2 p-2 items-center cursor-pointer', [userIcon, userText]);
+    const userText = createElement('span', 'text-gray-900 cursor-pointer group-hover:text-blue-700', 'Sign In');
+    const userWrapper = createElementWithChildren('div', 'hidden group flex gap-2 p-2 items-center cursor-pointer md:flex md:text-sm xl:text-base', [userIcon, userText]);
     const cartIcon = createFontAwesomeIcon(faShoppingCart, `${ICON_SIZE.medium} text-gray-900 cursor-pointer hover:text-blue-700`);
-    const actionsWrapper = createElementWithChildren('div', 'flex gap-10 items-center font-medium text-lg', [userWrapper, cartIcon]);
+    const cartBtn = createElementWithChildren('button', 'flex items-center justify-center text-gray-900 p-2', [cartIcon]);
+    const actionsWrapper = createElementWithChildren('div', 'flex gap-2 items-center justify-end font-medium text-lg', [loupeBtn, userWrapper, cartBtn]);
 
-    const header = createElementWithChildren('header', 'bg-white flex w-7xl h-fit justify-between items-center', [logo, nav, actionsWrapper]);
+    const header = createElementWithChildren('header', 'bg-white grid grid-cols-[1fr_3fr_1fr] h-fit px-4 my-4 md:grid-cols-[1fr_1.5fr_1fr] lg:flex lg:w-5xl lg:mx-auto lg:justify-between xl:w-7xl', [menuBtn, logo, nav, actionsWrapper]);
 
     return header;
 }   
