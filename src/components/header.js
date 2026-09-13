79141a48268ee7c086797a91c77577588c5563f4
Rusanov-Kirill <rusanovkirill39@gmail.com>
2026-09-13 22:53:34 +0600

    chore(header): delete unnecessary flex-tailwind classes

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ export function Header() {
     logo.src = ASSETS.logo;
     logo.alt = 'NovaCart Logo';
 
-    const nav = createElementWithChildren('nav', 'hidden flex justify-between items-center font-medium text-lg lg:flex lg:gap-6 xl:gap-12', [
+    const nav = createElementWithChildren('nav', 'hidden justify-between items-center font-medium text-lg lg:flex lg:gap-6 xl:gap-12', [
         ...NAV_LINKS.map(linkText => {
             const link = createElement('a', `flex p-2 cursor-pointer hover:text-blue-700 ${linkText === 'Home' ? 'relative text-blue-700 after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-blue-700' : 'text-gray-900'}`, linkText);
             return link;
@@ -23,7 +23,7 @@ export function Header() {
     const loupeBtn = createElementWithChildren('button', 'flex items-center justify-center text-gray-900 p-2', [loupeIcon]);
     const userIcon = createFontAwesomeIcon(faUser, `${ICON_SIZE.medium} text-gray-900 cursor-pointer group-hover:text-blue-700`);
     const userText = createElement('span', 'text-gray-900 cursor-pointer group-hover:text-blue-700', 'Sign In');
-    const userWrapper = createElementWithChildren('div', 'hidden group flex gap-2 p-2 items-center cursor-pointer md:flex md:text-sm xl:text-base', [userIcon, userText]);
+    const userWrapper = createElementWithChildren('div', 'hidden group gap-2 p-2 items-center cursor-pointer md:flex md:text-sm xl:text-base', [userIcon, userText]);
     const cartIcon = createFontAwesomeIcon(faShoppingCart, `${ICON_SIZE.medium} text-gray-900 cursor-pointer hover:text-blue-700`);
     const cartBadge = createElement('span', 'absolute flex -top-1 -right-1 bg-blue-700 text-white text-xs rounded-full w-5.5 h-5.5 items-center justify-center', '0');
     const cartBtn = createElementWithChildren('button', 'relative flex items-center justify-center text-gray-900 p-2', [cartIcon, cartBadge]);
